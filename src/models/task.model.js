15025eebeddaa5e7f2c100280ade9ff6ed1e3509
xmarchena50072 <xmarchena50072@ufide.ac.cr>
2024-05-30 15:37:25 -0600

    Completa crud  de tasks

--- src/models/task.model.js
+++ src/models/task.model.js
@@ -13,6 +13,11 @@ const taskSchema = new mongoose.Schema({
         type: Date,
         default: Date.now,
     },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
 }, { timestamps: true });
 
 export default mongoose.model("Task", taskSchema);
