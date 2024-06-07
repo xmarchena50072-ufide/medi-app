d314e94d143f30076e5cab4e26edcb25f4ef9a28
xmarchena50072 <xmarchena50072@ufide.ac.cr>
2024-06-06 19:33:09 -0600

    AuthContext for Register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { AuthProvider } from "./context/AuthContext";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<h1>Home page</h1>} />
-        <Route path="/login" element={<LoginPage></LoginPage>} />
-        <Route path="/register" element={<RegisterPage></RegisterPage>} />
-        <Route path="/tasks" element={<h1>Tasks page</h1>} />
-        <Route path="/add-task" element={<h1>New task</h1>} />
-        <Route path="/tasks/:id" element={<h1>Update task</h1>} />
-        <Route path="/profile" element={<h1>profile</h1>} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home page</h1>} />
+          <Route path="/login" element={<LoginPage></LoginPage>} />
+          <Route path="/register" element={<RegisterPage></RegisterPage>} />
+          <Route path="/tasks" element={<h1>Tasks page</h1>} />
+          <Route path="/add-task" element={<h1>New task</h1>} />
+          <Route path="/tasks/:id" element={<h1>Update task</h1>} />
+          <Route path="/profile" element={<h1>profile</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 };
 
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,7 +5,11 @@ import { createAccessToken } from "../libs/jwt.js";
 export const register = async (req, res) => {
     const { email, password, username } = req.body;
     try {
+        const userFound = await User.findOne({email});
+        if (userFound) return res.status(400).json(["the email is already in use"]);
+        
         const passwordHash = await bcrypt.hash(password, 10);
+        
         const newUser = new User({
             username,
             email,
