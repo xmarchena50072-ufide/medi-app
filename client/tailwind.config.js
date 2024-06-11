cf08baf7629a948cf82e792db2af9f8a93fe4685
xmarchena50072 <xmarchena50072@ufide.ac.cr>
2024-06-11 15:15:11 -0600

    Medical app styles

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      'blue': '#24648f;',
+      'blue-light': '#88C5E0;',
+      'red': '#EF4444',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
     extend: {},
   },
   plugins: [],
