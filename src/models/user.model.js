3ccc847d610d4c5711dde5ab77c420318e6429a3
xmarchena50072 <xmarchena50072@ufide.ac.cr>
2024-09-23 20:37:28 -0600

    Agrega campo rol a modelo de usuario

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -16,6 +16,10 @@ const userSchema = mongoose.Schema(
         password: {
             type: String,
             required: true
+        },
+        role: {
+            type: String,
+            required: true,
         }
     }
     , {
