4f37452a0d54e0c541992603164b16642e1d6d0a
xmarchena50072 <xmarchena50072@ufide.ac.cr>
2024-09-30 20:19:18 -0600

    Agrega role a client y controller

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -22,7 +22,7 @@ function RegisterPage() {
     <div className="flex h-[calc(100vh-100px)] items-center justify-center bg-gray-100">
       <div className="bg-gray-800 max-w-md p-10 rounded-md shadow-lg">
         <div className="flex justify-center mb-4">
-          <img src="/src/assets/corazon.svg" alt="Medi-App Logo" className="w-16 h-16"/>
+          <img src="/src/assets/corazon.svg" alt="Medi-App Logo" className="w-16 h-16" />
         </div>
         {registerErrors.length > 0 && (
           <div className="bg-red-600 text-white p-2 mb-4 rounded-md">
@@ -59,6 +59,17 @@ function RegisterPage() {
           {errors.password && (
             <p className="text-red-500 text-sm mb-4">{t('registerPage.passwordRequired')}</p>
           )}
+          {/* Rol*/}
+          <input
+            type="text"
+            {...register("role", { required: true })}
+            className="w-full bg-white px-4 py-2 rounded-md mb-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder={t('registerPage.rolePlaceholder')}
+          />
+          {errors.role && (
+            <p className="text-red-500 text-sm mb-4">{t('registerPage.roleRequired')}</p>
+          )}
+
           <button className="w-full bg-blue text-white px-4 py-2 rounded-md hover:bg-blue transition duration-300" type="submit">
             {t('registerPage.registerButton')}
           </button>
